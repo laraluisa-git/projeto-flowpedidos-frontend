1dc0339cf2c9a5d46a16d672797aa9528d8608ec
Lara Luisa <larafelix30@gmail.com>
2026-03-25 09:28:59 -0300

    Remove DataProvider from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <DataProvider>
           <App />
-        <DataProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
